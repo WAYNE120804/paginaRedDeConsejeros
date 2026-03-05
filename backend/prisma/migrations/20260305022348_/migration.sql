ebe472c0806d3a4552daaca8602b7ced0830e8df
jhon sebastian diaz <jhonsebastian-04@hotmail.com>
2026-03-04 21:49:30 -0500

    se elimino migracion corrupta

--- backend/prisma/migrations/20260305022348_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "event_photos" DROP CONSTRAINT "event_photos_event_id_fkey";
-
--- AddForeignKey
-ALTER TABLE "event_photos" ADD CONSTRAINT "event_photos_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
