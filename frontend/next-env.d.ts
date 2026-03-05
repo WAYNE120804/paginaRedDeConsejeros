f4470e888ad3b9c90e48940a1d62b57ac0a8139f
jhon sebastian diaz <jhonsebastian-04@hotmail.com>
2026-03-05 10:36:17 -0500

    c

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -2,3 +2,16 @@
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
+<<<<<<< ours
+<<<<<<< ours
+<<<<<<< ours
+<<<<<<< ours
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
+=======
+>>>>>>> theirs
+=======
+>>>>>>> theirs
+=======
+>>>>>>> theirs
+=======
+>>>>>>> theirs
